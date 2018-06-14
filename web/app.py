542da9b66e25477d46b3fabd605d0751fd9e3396
Roman Yurkevych <romuriy@gmail.com>
2018-06-14 10:00:08 +0200

    Added Bootstrap

--- web/app.py
+++ web/app.py
@@ -6,8 +6,10 @@ from flask import Flask
 from flask import render_template, request, flash
 from media.s3_storage import S3MediaStorage
 from media.generate_name import generate_name
+from flask_bootstrap import Bootstrap
  
 app = Flask(__name__)
+Bootstrap(app)
 
 s3 = boto3.resource('s3')
 media_storage = S3MediaStorage(s3, os.getenv('APP_BUCKET_NAME'))
