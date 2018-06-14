89c0efe4e08b3dcebba3ca0269f642e2929957e0
Roman Yurkevych <romuriy@gmail.com>
2018-06-14 10:59:18 +0200

    rewrite app.py

--- web/app.py
+++ web/app.py
@@ -1,13 +1,11 @@
 import boto3
 import os
 import json
-
 from flask import Flask
 from flask import render_template, request, flash
 from media.s3_storage import S3MediaStorage
-from media.generate_name import generate_name
+from media.name_generator import generate_name
 from flask_bootstrap import Bootstrap
- 
 app = Flask(__name__)
 Bootstrap(app)
 
@@ -18,33 +16,32 @@ photos_list=[]
 
 sqs = boto3.resource('sqs', region_name="eu-central-1")
 requestQueue = sqs.get_queue_by_name(
-  QueueName=os.getenv('APP_QUEUE_NAME')
+  QueueName=os.getenv("APP_QUEUE_NAME")
 )
+
 
 @app.route("/")
 def hello():
     return render_template(
       'upload_files.html'
     )
-
 @app.route("/upload", methods=['POST'])
 def handle_upload():
   if 'uploaded_file' not in request.files:
     flash('No file part')
     return redirect(request.url)
-
-  upload_file = request.files['uploaded_file']
-  file_ref = generate_name(upload_file.filename)
+  
+  uploaded_file = request.files['uploaded_file']
+  destination_name = generate_name(uploaded_file.filename)
   media_storage.store(
-     dest=file_ref,
-     source=upload_file
+     dest=destination_name,
+     source=uploaded_file
   )
 
-  photos_list.append(file_ref)
-
+  photos_list.append(destination_name)
 
   return prepare()
-
+ 
 @app.route("/proceed", methods=["POST"])
 def proceed_animation():
   ani_request = {
@@ -55,15 +52,18 @@ def proceed_animation():
   requestQueue.send_message(
     MessageBody=json.dumps(ani_request)
   )
-  return "OK"
+
+  return render_template(
+      'proceed.html'
+    )
 
 @app.route("/prepare")
 def prepare():
   return render_template(
     'prepare.html',
-    invitation="aaathe only limit is yourself",
-    photos=photos_list
-)
+    invitation="Photo list",
+    photos=photos_list 
+  )
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=True)
