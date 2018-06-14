64babbfd2c9b0df6ca5afdbfb8bf34dd9461f5dd
Roman Yurkevych <romuriy@gmail.com>
2018-06-14 10:06:15 +0200

    Update user.py

--- user.py
+++ user.py
@@ -3,9 +3,9 @@ import json, boto3, uuid
 sqs = boto3.resource('sqs', region_name="eu-central-1")
 
 tweets = sqs.get_queue_by_name(QueueName='204153RY')
-response = tweets.send_message(MessageBody='Hello World', MessageAttributes={
-    'Author': {
-        'StringValue': 'Jakub',
-        'DataType': 'String'
-    }
-})
+
+while True:
+  for message in tweets.receive_messages():
+    print('Message body: %s' % message.body)
+    message.delete()
+  time.sleep(1)
